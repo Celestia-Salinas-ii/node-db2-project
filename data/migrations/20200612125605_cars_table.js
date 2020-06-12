97fc6413b5a6eb1e538fcf35ac46c325e01952fb
Celestia Salinas <csalinas3671@gmail.com>
2020-06-12 13:24:57 -0400

    mvp

---
+++ data/migrations/20200612125605_cars_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.integer("VIN").notNullable()
+        tbl.string("make", 200).notNullable()
+        tbl.string("model", 200).notNullable()
+        tbl.integer("mileage").notNullable()
+        tbl.string("transmissionType");
+        tbl.string("statusOfTitle");
+  
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars')
+  };
